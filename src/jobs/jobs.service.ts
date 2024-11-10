3d81dd9a7f19ad13b7544f69034d309277c1247a
tomer-tgp <tomer@goodparty.org>
2024-11-09 21:42:00 -0500

    fix: use http service

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,14 +1,17 @@
+import { HttpService } from '@nestjs/axios'
 import { Injectable } from '@nestjs/common'
-import axios from 'axios'
+import { firstValueFrom } from 'rxjs'
 
 const API_BASE = 'https://api.ashbyhq.com/jobPosting'
 const ASHBEY_KEY = process.env.ASHBEY_KEY
 
 @Injectable()
 export class JobsService {
+  constructor(private readonly httpService: HttpService) {}
+
   async findAll() {
     try {
-      const jobs = await fetchJobs('list', { listedOnly: true })
+      const jobs = await this.fetchJobs('list', { listedOnly: true })
       return jobs || []
     } catch (error) {
       console.error('Error during fetch:', error.message)
@@ -18,7 +21,7 @@ export class JobsService {
 
   async findOne(id: string) {
     try {
-      const job = await fetchJobs('info', { jobPostingId: id })
+      const job = await this.fetchJobs('info', { jobPostingId: id })
       if (!job) {
         return null
       }
@@ -28,18 +31,20 @@ export class JobsService {
       return null
     }
   }
-}
 
-async function fetchJobs(type: string, params?: any) {
-  const url = `${API_BASE}.${type}`
-  const response = await axios.post(url, params, {
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization: `Basic ${Buffer.from(ASHBEY_KEY + ':').toString(
-        'base64',
-      )}`,
-    },
-  })
-  return response?.data?.results
+  async fetchJobs(type: string, params?: any) {
+    const url = `${API_BASE}.${type}`
+    const response = await firstValueFrom(
+      this.httpService.post(url, params, {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Basic ${Buffer.from(ASHBEY_KEY + ':').toString(
+            'base64',
+          )}`,
+        },
+      }),
+    )
+    return response.data.results
+  }
 }
