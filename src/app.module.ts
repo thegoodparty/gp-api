35a4d13084903f461586b75296827b778d0fc9b1
tomer-tgp <tomer@goodparty.org>
2024-11-13 22:59:17 -0500

    feat: models for races, county and municipality

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service'
 import { ContentModule } from './content/content.module'
 import { JobsModule } from './jobs/jobs.module'
 import { HealthModule } from './health/health.module'
+import { RacesModule } from './races/races.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [ContentModule, JobsModule, HealthModule],
+  imports: [ContentModule, JobsModule, HealthModule, RacesModule],
 })
 export class AppModule {}
