520ad9b160c04fdeb8e615b3cdf526c15dd30149
Sanjeev Nayak <snayak00@gmail.com>
2024-11-21 15:08:49 -0500

    WEB-3356: basic factories + seed data

--- api/seed/campaigns.ts
+++ api/seed/campaigns.ts
@@ -1,19 +1,45 @@
 import { PrismaClient } from '@prisma/client'
-import { fakeCampaignComplete } from '../prisma/faker'
+import {
+  campaignFactory,
+  campaignUpdateHistoryFactory,
+  pathToVictoryFactory,
+} from './factories'
 
 const NUM_CAMPAIGNS = 20
-
-main()
+const NUM_UPDATE_HISTORY = 3
 
 async function main() {
   const prisma = new PrismaClient()
   const fakeCampaigns = []
+  const fakeP2Vs = []
+  const fakeUpdateHistory = []
 
   for (let i = 0; i < NUM_CAMPAIGNS; i++) {
-    fakeCampaigns[i] = fakeCampaignComplete()
+    const camp = campaignFactory()
+    const p2v = pathToVictoryFactory({ campaignId: camp.id })
+
+    for (let j = 0; j < NUM_UPDATE_HISTORY; j++) {
+      fakeUpdateHistory[NUM_UPDATE_HISTORY * i + j] =
+        campaignUpdateHistoryFactory({
+          campaignId: camp.id,
+        })
+    }
+
+    fakeCampaigns[i] = camp
+    fakeP2Vs[i] = p2v
   }
 
   const { count } = await prisma.campaign.createMany({ data: fakeCampaigns })
+  const { count: p2vCount } = await prisma.pathToVictory.createMany({
+    data: fakeP2Vs,
+  })
+  const { count: historyCount } = await prisma.campaignUpdateHistory.createMany(
+    { data: fakeUpdateHistory },
+  )
 
-  console.log(`Created ${count} campaigns`)
+  console.log(
+    `Created ${count} campaigns, with ${p2vCount} paths to victory, and ${historyCount} campaign update history items`,
+  )
 }
+
+main()
---
+++ api/seed/factories.ts
@@ -0,0 +1,70 @@
+import {
+  Campaign,
+  CampaignTier,
+  CampaignUpdateHistory,
+  CampaignUpdateHistoryType,
+  PathToVictory,
+} from '@prisma/client'
+import { faker } from '@faker-js/faker'
+import { deepMerge } from '../src/shared/helpers/objectHelper'
+
+/**
+ * Helper to make a factory function that merges a default generator with a custom props object
+ * @param generateFn Function to generate a default mock entity
+ * @returns Factory function that accepts args to override default generated values
+ * @example
+ * const userFactory = generateFactory<User>(() => ({
+ *   id: faker.number.int(),
+ *   firstName: faker.person.firstName(),
+ *   lastName: faker.person.lastName()
+ * }))
+ *
+ * const fakeUser = userFactory({ firstName: 'Jerry'})
+ */
+function generateFactory<T>(generateFn: (args?: unknown) => Partial<T>) {
+  return (args: Partial<T> = {}) => deepMerge(generateFn(), args) as T
+}
+
+export const campaignFactory = generateFactory<Campaign>(() => ({
+  id: faker.number.int({ max: 2147483647 }),
+  createdAt: new Date(),
+  updatedAt: faker.date.anytime(),
+  slug: faker.lorem.words(5),
+  isActive: faker.datatype.boolean(0.5),
+  isVerified: faker.datatype.boolean(0.5),
+  isPro: faker.datatype.boolean(0.5),
+  isDemo: faker.datatype.boolean(0.1),
+  didWin: undefined,
+  dateVerified: undefined,
+  tier: faker.helpers.arrayElement(Object.values(CampaignTier)),
+  data: {},
+  details: {},
+  aiContent: {},
+  vendorTsData: {},
+}))
+
+export const pathToVictoryFactory = generateFactory<PathToVictory>(() => ({
+  id: faker.number.int({ max: 2147483647 }),
+  createdAt: new Date(),
+  updatedAt: faker.date.anytime(),
+  campaignId: faker.number.int(),
+  data: JSON.stringify({
+    foo: 'dc2f6c7c-ad6a-408b-9b6e-e273280b572d',
+    bar: 8466451537068032,
+    bike: 'd',
+    a: 'T',
+    b: 0.3825467659626156,
+    name: 'Jewel',
+    prop: '0b1',
+  }),
+}))
+
+export const campaignUpdateHistoryFactory =
+  generateFactory<CampaignUpdateHistory>(() => ({
+    id: faker.number.int({ max: 2147483647 }),
+    createdAt: new Date(),
+    updatedAt: faker.date.anytime(),
+    campaignId: faker.number.int(),
+    type: faker.helpers.arrayElement(Object.values(CampaignUpdateHistoryType)),
+    quantity: faker.number.int({ min: 100, max: 10000 }),
+  }))
