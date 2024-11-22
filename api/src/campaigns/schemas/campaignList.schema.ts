83a7859b09bfc8b7ca18d59cb4837a7db04e6d5f
Sanjeev Nayak <snayak00@gmail.com>
2024-11-22 17:07:16 -0500

    WEB-3356: fix zod types

--- api/src/campaigns/schemas/campaignList.schema.ts
+++ api/src/campaigns/schemas/campaignList.schema.ts
@@ -2,18 +2,18 @@ import { isState } from 'src/shared/validations/isState'
 import { z } from 'zod'
 
 export const campaignListSchema = z.object({
-  id: z.number().optional(),
+  id: z.coerce.number().optional(),
   state: isState()
     .transform((val) => val.toUpperCase())
     .optional(),
   email: z.string().email().optional(),
   slug: z.string().optional(),
   level: z.enum(['LOCAL', 'CITY', 'COUNTY', 'STATE', 'FEDERAL']).optional(),
-  primaryElectionDateStart: z.string().datetime().optional(),
-  primaryElectionDateEnd: z.string().datetime().optional(),
+  primaryElectionDateStart: z.string().date().optional(),
+  primaryElectionDateEnd: z.string().date().optional(),
   campaignStatus: z.enum(['active', 'inactive']).optional(),
-  generalElectionDateStart: z.string().datetime().optional(),
-  generalElectionDateEnd: z.string().datetime().optional(),
+  generalElectionDateStart: z.string().date().optional(),
+  generalElectionDateEnd: z.string().date().optional(),
   p2vStatus: z.string().optional(),
 })
 
