b1a4d88944959e63ead7f5d1722e78438d535c53
Sanjeev Nayak <snayak00@gmail.com>
2024-12-03 11:11:05 -0500

    WEB-3357: add user role enum

---
+++ prisma/migrations/20241203161031_user_role_enum/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - The `role` column on the `user` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('sales', 'campaign');
+
+-- AlterTable
+ALTER TABLE "user" DROP COLUMN "role",
+ADD COLUMN     "role" "UserRole";
