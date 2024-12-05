570d317777575cc777d44e75b7bf0859b19267d1
oakinh <nikaohill@gmail.com>
2024-12-05 10:42:22 -0600

    Remove ID from UpdateTopIssueSchema

--- src/top-issues/schemas/topIssues.schema.ts
+++ src/top-issues/schemas/topIssues.schema.ts
@@ -2,13 +2,11 @@ import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 export const UpdateTopIssueSchema = z.object({
-  id: z.number(),
   name: z.string(),
   icon: z.string().nullable(),
 })
 .strict()
 export class UpdateTopIssueDto extends createZodDto(UpdateTopIssueSchema) {};
-
 
 export const CreateTopIssueSchema = z.object({ 
   name: z.string(), 
