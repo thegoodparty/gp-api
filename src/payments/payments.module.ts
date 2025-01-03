61a6d1d97d58f9e2acdf27107280fbee8687e299
Matthew Marcus <lurchbulldog@gmail.com>
2025-01-03 16:00:22 -0600

    [WEB-3475] - created controller for webhook endpoint

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { StripeService } from './stripe/stripe.service'
 import { PurchaseController } from './purchase/purchase.controller'
 import { UsersModule } from '../users/users.module'
+import { PaymentsController } from './payments/payments.controller';
 
 @Module({
   providers: [StripeService],
-  controllers: [PurchaseController],
+  controllers: [PurchaseController, PaymentsController],
   imports: [UsersModule],
 })
 export class PaymentsModule {}
