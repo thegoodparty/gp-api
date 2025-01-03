09bde9629352c611da4f08a71830804a93bdbb39
Matthew Marcus <lurchbulldog@gmail.com>
2025-01-03 14:52:37 -0600

    [WEB-3387] - Create Payment Portal Session

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export {}
+
+// TODO: figure out if it's possible to collocate these Json types with the
+//  models they're associated with
+declare global {
+  export namespace PrismaJson {
+    type UserMetaData = {
+      customerId?: string
+      checkoutSessionId?: string
+    }
+  }
+}
