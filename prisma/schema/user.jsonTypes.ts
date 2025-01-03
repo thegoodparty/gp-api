59dd06a917120d5634fa4901c14127daeef88c9f
Matthew Marcus <lurchbulldog@gmail.com>
2025-01-03 15:55:02 -0600

    [WEB-3388] - splits out json type files and collocates them w/ prisma schemas

--- prisma/schema/user.jsonTypes.ts
+++ prisma/schema/user.jsonTypes.ts
@@ -1,10 +1,8 @@
 export {}
 
-// TODO: figure out if it's possible to collocate these Json types with the
-//  models they're associated with
 declare global {
   export namespace PrismaJson {
-    type UserMetaData = {
+    export type UserMetaData = {
       customerId?: string
       checkoutSessionId?: string
     } | null
