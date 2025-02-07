bfca5b7c531dd5cc76ce781adb28e2e7f3108daf
oakinh <nikaohill@gmail.com>
2025-02-07 13:05:29 -0600

    Some type changes:

--- src/races/types/ballotReady.types.ts
+++ src/races/types/ballotReady.types.ts
@@ -1,18 +1,29 @@
 import { Election, PageInfo, Position } from 'src/generated/graphql.types'
 import { BDElection } from './ballotData.types'
 
-export type RacesByZipcode = {
-  races: {
-    edges: RaceEdge[]
-    pageInfo: PageInfo
-  }
+// -----------------------------
+// Shared/Generic Types
+// -----------------------------
+
+export interface PaginatedResponse<T> {
+  edges: { node: T }[]
+  pageInfo: PageInfo
 }
 
-type RaceEdge = {
-  node: RaceNode
+export interface FilingPeriod {
+  startOn: string
+  endOn: string
 }
 
-export type RaceNode = {
+// -----------------------------
+// Races By Zipcode Types
+// -----------------------------
+
+export interface RacesByZipcode {
+  races: PaginatedResponse<RaceNode>
+}
+
+export interface RaceNode {
   id: string
   isPrimary: boolean
   filingPeriods: FilingPeriod[]
@@ -20,17 +31,12 @@ export type RaceNode = {
   position: RacesByZipcodePosition
 }
 
-type FilingPeriod = {
-  startOn: string
-  endOn: string
-}
-
-type RacesByZipcodeElection = Pick<
+export type RacesByZipcodeElection = Pick<
   Election,
   'id' | 'electionDay' | 'name' | 'originalElectionDate' | 'state' | 'timezone'
 >
 
-type RacesByZipcodePosition = Pick<
+export type RacesByZipcodePosition = Pick<
   Position,
   | 'id'
   | 'appointed'
@@ -45,51 +51,36 @@ type RacesByZipcodePosition = Pick<
   | 'electionFrequencies'
 >
 
-// type Position = {
-//   id: string
-//   appointed: false
-//   hasPrimary: true
-//   partisanType: PartisanType
-//   level: PositionLevel
-//   name: string
-//   salary: string
-//   state: string
-//   subAreaName: string
-//   subAreaValue: string
-//   electionFrequencies: ElectionFrequency[]
-// }
+// -----------------------------
+// Races With Election Dates Types
+// -----------------------------
 
-// type ElectionFrequency = {
-//   frequency: number[]
-// }
-
-type BasicRaceEdge = {
-  node: BasicRaceNode[]
-}
-
-type BasicRaceNode = {
+interface BasicRaceNode {
   position: Pick<Position, 'name'>
   election: Pick<Election, 'electionDay'>
 }
 
-export type RacesWithElectionDates = {
-  races: {
-    edges: BasicRaceEdge[]
-    pageInfo: PageInfo
-  }
+interface BasicRaceEdge {
+  node: BasicRaceNode
 }
 
-export type RacesById = {
-  races: {
-    edges: RacesByIdEdge[]
-  }
+export interface RacesWithElectionDates {
+  races: PaginatedResponse<BasicRaceEdge>
 }
 
-type RacesByIdEdge = {
+// -----------------------------
+// Races By Id Types
+// -----------------------------
+
+export interface RacesById {
+  races: PaginatedResponse<RacesByIdEdge>
+}
+
+interface RacesByIdEdge {
   node: RacesByIdNode
 }
 
-type RacesByIdNode = {
+interface RacesByIdNode {
   databaseId: string
   isPartisan: boolean
   isPrimary: boolean
@@ -118,3 +109,103 @@ type RacesByIdPosition = Pick<
   | 'electionFrequencies'
   | 'hasPrimary'
 >
+
+// export type RacesByZipcode = {
+//   races: {
+//     edges: RaceEdge[]
+//     pageInfo: PageInfo
+//   }
+// }
+
+// type RaceEdge = {
+//   node: RaceNode
+// }
+
+// export type RaceNode = {
+//   id: string
+//   isPrimary: boolean
+//   filingPeriods: FilingPeriod[]
+//   election: RacesByZipcodeElection | BDElection
+//   position: RacesByZipcodePosition
+// }
+
+// type FilingPeriod = {
+//   startOn: string
+//   endOn: string
+// }
+
+// type RacesByZipcodeElection = Pick<
+//   Election,
+//   'id' | 'electionDay' | 'name' | 'originalElectionDate' | 'state' | 'timezone'
+// >
+
+// type RacesByZipcodePosition = Pick<
+//   Position,
+//   | 'id'
+//   | 'appointed'
+//   | 'hasPrimary'
+//   | 'partisanType'
+//   | 'level'
+//   | 'name'
+//   | 'salary'
+//   | 'state'
+//   | 'subAreaName'
+//   | 'subAreaValue'
+//   | 'electionFrequencies'
+// >
+
+// type BasicRaceEdge = {
+//   node: BasicRaceNode[]
+// }
+
+// type BasicRaceNode = {
+//   position: Pick<Position, 'name'>
+//   election: Pick<Election, 'electionDay'>
+// }
+
+// export type RacesWithElectionDates = {
+//   races: {
+//     edges: BasicRaceEdge[]
+//     pageInfo: PageInfo
+//   }
+// }
+
+// export type RacesById = {
+//   races: {
+//     edges: RacesByIdEdge[]
+//   }
+// }
+
+// type RacesByIdEdge = {
+//   node: RacesByIdNode
+// }
+
+// type RacesByIdNode = {
+//   databaseId: string
+//   isPartisan: boolean
+//   isPrimary: boolean
+//   filingPeriods: FilingPeriod[]
+//   election: RacesByIdElection
+//   position: RacesByIdPosition
+// }
+
+// type RacesByIdElection = Pick<Election, 'electionDay' | 'name' | 'state'>
+
+// type RacesByIdPosition = Pick<
+//   Position,
+//   | 'id'
+//   | 'description'
+//   | 'judicial'
+//   | 'level'
+//   | 'name'
+//   | 'partisanType'
+//   | 'staggeredTerm'
+//   | 'state'
+//   | 'subAreaName'
+//   | 'subAreaValue'
+//   | 'tier'
+//   | 'mtfcc'
+//   | 'geoId'
+//   | 'electionFrequencies'
+//   | 'hasPrimary'
+// >
