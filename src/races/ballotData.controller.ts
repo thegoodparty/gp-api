ba5974b19e506f719c036488beef80e7e6acdb46
oakinh <nikaohill@gmail.com>
2025-02-07 14:57:27 -0600

    Remove code for testing

--- src/races/ballotData.controller.ts
+++ src/races/ballotData.controller.ts
@@ -5,23 +5,10 @@ import { BallotDataService } from './services/ballotData.service'
 @Controller('ballotdata')
 @PublicAccess()
 export class BallotDataController {
-  constructor(
-    private readonly ballotDataService: BallotDataService,
-    // private readonly ballotReadyService: BallotReadyService,
-  ) {}
+  constructor(private readonly ballotDataService: BallotDataService) {}
 
   @Get('races-by-year')
   async getRacesByZipcode(@Query('zipcode') zipcode: string) {
     return await this.ballotDataService.getRacesByZipcode(zipcode)
   }
-
-  // @Get('test-other-endpoint')
-  // async testOtherEndpoint(
-  //   @Query() query: { zipcode: string; positionLevel: PositionLevel },
-  // ) {
-  //   return await this.ballotReadyService.fetchRacesWithElectionDates(
-  //     query.zipcode,
-  //     query.positionLevel,
-  //   )
-  // }
 }
