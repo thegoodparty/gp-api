1c527521f4b22cf93befd690cf8cf273dd24a92f
oakinh <nikaohill@gmail.com>
2025-02-07 13:53:11 -0600

    Remove unused import

--- src/races/ballotData.controller.ts
+++ src/races/ballotData.controller.ts
@@ -1,15 +1,11 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import { PublicAccess } from 'src/authentication/decorators/PublicAccess.decorator'
 import { BallotDataService } from './services/ballotData.service'
-import { BallotReadyService } from './services/ballotReadyservice'
 
 @Controller('ballotdata')
 @PublicAccess()
 export class BallotDataController {
-  constructor(
-    private readonly ballotDataService: BallotDataService,
-    private readonly ballotReadyService: BallotReadyService,
-  ) {}
+  constructor(private readonly ballotDataService: BallotDataService) {}
 
   @Get('races-by-year')
   async getRacesByZipcode(@Query('zipcode') zipcode: string) {
