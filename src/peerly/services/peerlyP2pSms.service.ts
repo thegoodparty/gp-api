8687eb6045fefd7ce069e977c1c52127dcd7ed96
Israel Vicars <israelvicars@gmail.com>
2025-08-27 15:28:37 -0400

    fix: default params, paths for peerly

--- src/peerly/services/peerlyP2pSms.service.ts
+++ src/peerly/services/peerlyP2pSms.service.ts
@@ -65,7 +65,6 @@ export class PeerlyP2pSmsService extends PeerlyBaseConfig {
       account_id: this.accountNumber,
       name,
       templates,
-      did_state: didState,
       can_use_mms: hasMms,
       ...(identityId && { identity_id: identityId }),
     }
@@ -73,7 +72,7 @@ export class PeerlyP2pSmsService extends PeerlyBaseConfig {
     try {
       const config = await this.getBaseHttpHeaders()
       const response = await lastValueFrom(
-        this.httpService.post(`${this.baseUrl}/api/1to1/jobs`, body, config),
+        this.httpService.post(`${this.baseUrl}/1to1/jobs`, body, config),
       )
 
       const validated = this.validateCreateJobResponse(response.data)
@@ -122,7 +121,7 @@ export class PeerlyP2pSmsService extends PeerlyBaseConfig {
       const config = await this.getBaseHttpHeaders()
       await lastValueFrom(
         this.httpService.post(
-          `${this.baseUrl}/api/1to1/jobs/${jobId}/assignlist`,
+          `${this.baseUrl}/1to1/jobs/${jobId}/assignlist`,
           body,
           config,
         ),
@@ -144,7 +143,7 @@ export class PeerlyP2pSmsService extends PeerlyBaseConfig {
       const config = await this.getBaseHttpHeaders()
       await lastValueFrom(
         this.httpService.post(
-          `${this.baseUrl}/api/v2/p2p/${jobId}/request_canvassers`,
+          `${this.baseUrl}/v2/p2p/${jobId}/request_canvassers`,
           body,
           config,
         ),
