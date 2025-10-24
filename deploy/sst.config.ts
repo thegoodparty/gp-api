aa6d7e441abcb95cba3ca1577a410a52deeca74d
Stephen <stephen.tanguis@gmail.com>
2025-10-24 16:26:16 -0500

    Update syntax for healthcheck target
    Update syntax for healthcheck target

--- deploy/sst.config.ts
+++ deploy/sst.config.ts
@@ -376,6 +376,13 @@ export default $config({
         loadBalancer: {
           idleTimeout: 120,
         },
+        target: (targetArgs) => {
+          targetArgs.healthCheck = {
+            ...targetArgs.healthCheck,
+            path: '/v1/health',
+            interval: 30,
+          }
+        },
       },
       permissions: [
         {
