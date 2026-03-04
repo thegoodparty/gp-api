d1c9e24e250bb0d634482924f7a919acad78fd97
Stephen <stephen.tanguis@gmail.com>
2026-03-04 09:47:54 -0600

    Add backfill for organizations
    Add backfill for organizations

--- src/organizations/organizations.module.ts
+++ src/organizations/organizations.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { OrganizationsService } from './services/organizations.service'
+import { OrganizationsBackfillService } from './services/organizations-backfill.service'
 import { ElectionsModule } from '@/elections/elections.module'
 import { OrganizationsController } from './organizations.controller'
 
 @Module({
   imports: [ElectionsModule],
-  providers: [OrganizationsService],
+  providers: [OrganizationsService, OrganizationsBackfillService],
   controllers: [OrganizationsController],
   exports: [OrganizationsService],
 })
