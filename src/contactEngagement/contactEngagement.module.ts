31382699bae378535d4d64ce1779f3c82f6a1ad1
Stephen <stephen.tanguis@gmail.com>
2026-03-10 15:13:28 -0500

    module adjustment
    module adjustment

--- src/contactEngagement/contactEngagement.module.ts
+++ src/contactEngagement/contactEngagement.module.ts
@@ -1,11 +1,11 @@
-import { OrganizationsModule } from '@/organizations/organizations.module'
+import { ElectedOfficeModule } from '@/electedOffice/electedOffice.module'
 import { PollsModule } from '@/polls/polls.module'
 import { Module } from '@nestjs/common'
 import { ContactEngagementController } from './contactEngagement.controller'
 import { ContactEngagementService } from './contactEngagement.service'
 
 @Module({
-  imports: [OrganizationsModule, PollsModule],
+  imports: [ElectedOfficeModule, PollsModule],
   controllers: [ContactEngagementController],
   providers: [ContactEngagementService],
   exports: [ContactEngagementService],
